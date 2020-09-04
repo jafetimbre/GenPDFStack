f123fc2c7bd93eb01f2022352a953ff6569dfd3d
jafetimbre <jafet.imbre@gmail.com>
2020-09-04 19:00:17 +0300

    project base setup
    Created a blank CLI tool using the Click library

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='GenPDFStack',
+    version='1.0',
+    py_modules=['genPDFStack'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        genpdfstack=genPDFStack:main
+    '''
+)
